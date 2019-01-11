48953898fba4a4e608e8d9d33cc4d041b0d778a8
Nicholas Doyle <nickdoyle98@gmail.com>
2019-01-11 19:48:43 +0000

    chalk exercise

---
+++ notes.js
@@ -0,0 +1,16 @@
+// API -- What does API mean?
+
+/*
+-is a program you can use in your website
+-snippet of ccode
+-driver for separate apps BY JOSH
+-giphy giffer
+*/
+
+module.exports = 12
+
+console.log(module.exports);
+
+var imports = require('./test');
+
+console.log(imports);
---
+++ test.js
@@ -0,0 +1,7 @@
+module.exports = {
+  ["Scotch", "Cigars", "Diamonds", "Sports Cars"],
+  sayHi: function () {
+    console.log("sup")
+  }
+
+console.log("I'm here man");
